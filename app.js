c523b8c4a23205b73891bab20bdfffa031458698
TimeUpS <32740111+TimeUpS@users.noreply.github.com>
2020-12-05 19:09:49 +0330

    Update app.js

--- app.js
+++ app.js
@@ -33,13 +33,9 @@ bot.on("ready", () => {
     status: 'idle',
    
     })
-		               const channel = bot.channels.cache.get("784668009150021652");
-        if (!channel) return console.error("I did not find the vesta!");
-        channel.join().then(connection => {
-            console.log("I entered the Vesta channel| Xp ");
-        }).catch(e => {
-            console.error(e);
-        });
+		              
+
+        
     cron.schedule('*/15 * * * *', () => {
         var currentdate = new Date(); 
         var datetime = currentdate.getDate() + "/"
